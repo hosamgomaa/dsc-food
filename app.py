b4365321b55f9b00f19bb332ee26c8e7945dd0cc
Hossam Gomaa <hossamgomaa211@gmail.com>
2021-01-19 03:10:26 +0200

    final edition

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:123@localhost:5432/food"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+f_food=db.Table('famous_food',db.metadata,autoload=True,autoload_with=db.engine)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/search')
+@app.route('/search', methods=["POST"])
 def search():
-    return "bye"
+    city = request.form['city']
+    city = db.session.query(f_food).filter(f_food.c.city==city).values('food')
+    return render_template('results.html', food_names=city)
+
+    
 
 
-
-# if __name__ =="__main__":
-#     app.run(debug=True)
+if __name__ =="__main__":
+    app.run(debug=True)
